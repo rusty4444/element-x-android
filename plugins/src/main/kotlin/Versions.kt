abdee10f951d2609a4c84a272209bec90bcb2a63
rusty4444 <rusty4444@users.noreply.github.com>
2026-05-14 11:27:14 +1000

    Bump version to 0.8.6 for v0.8.6 release

--- plugins/src/main/kotlin/Versions.kt
+++ plugins/src/main/kotlin/Versions.kt
@@ -39,13 +39,13 @@ private const val versionYear = 26
  * Month of the version on 2 digits. Value must be in [1,12].
  * Do not update this value. it is updated by the release script.
  */
-private const val versionMonth = 5
+private const val versionMonth = 6
 
 /**
  * Release number in the month. Value must be in [0,99].
  * Do not update this value. it is updated by the release script.
  */
-private const val versionReleaseNumber = 16
+private const val versionReleaseNumber = 0
 
 object Versions {
     /**
@@ -55,7 +55,7 @@ object Versions {
      * See comment above for the calculation method.
      */
     const val VERSION_CODE = (2000 + versionYear) * 10_000 + versionMonth * 100 + versionReleaseNumber
-    const val VERSION_NAME = "0.8.5"
+    const val VERSION_NAME = "0.8.6"
 
     /**
      * Compile SDK version. Must be updated when a new Android version is released.

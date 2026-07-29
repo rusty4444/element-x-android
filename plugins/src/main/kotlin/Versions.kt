97837bf2177d9906535f07b9385fb68360393856
rusty4444 <rusty4444@users.noreply.github.com>
2026-07-29 13:10:41 +1000

    Release v0.10.12

--- plugins/src/main/kotlin/Versions.kt
+++ plugins/src/main/kotlin/Versions.kt
@@ -45,7 +45,7 @@ private const val versionMonth = 6
  * Release number in the month. Value must be in [0,99].
  * Do not update this value. it is updated by the release script.
  */
-private const val versionReleaseNumber = 23
+private const val versionReleaseNumber = 24
 
 object Versions {
     /**
@@ -55,7 +55,7 @@ object Versions {
      * See comment above for the calculation method.
      */
     const val VERSION_CODE = (2000 + versionYear) * 10_000 + versionMonth * 100 + versionReleaseNumber
-    const val VERSION_NAME = "0.10.11"
+    const val VERSION_NAME = "0.10.12"
 
     /**
      * Compile SDK version. Must be updated when a new Android version is released.
@@ -92,7 +92,7 @@ object Versions {
 
     /**
      * Java version used for compilation.
-     * Update this value when you want to use a newer Java version.
+     * Update this value when you want to use a newer version.
      */
     private const val JAVA_VERSION = 21
 
